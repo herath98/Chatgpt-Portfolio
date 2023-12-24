c68380a8bd50ef2a7b1c82ef2e859c99579438b0
harsha <harshaudayanga401@gmail.com>
2023-12-24 13:00:16 +0530

    modi

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import Grid from '@mui/material/Grid';
 import { styled } from '@mui/system';
 import cal from './images/s6.png';
 import shop from './images/s7.png';
-import hel from './images/s1.png';
+import helth from './images/s1.png';
 import fl from './images/s2.png';
 import hl from './images/s4.png';
 import li from './images/s5.png';
@@ -17,7 +17,7 @@ const projectsData = [
     id: 1,
     type: 'backend',
     name: 'Helthmat-predict pro',
-    image: hel,
+    image: helth,
     tools: ['Django', 'PostgreSQL', 'REST API', 'Jquery', 'Machine Lerning', 'Bootstrap', 'HTML&CSS'],
   },
   {
